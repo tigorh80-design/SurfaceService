69e7598603cd56d65237c18ce4d7618e95afa656
Igor Timofeyev <tigorh80@outlook.com>
2025-10-09 09:26:20 -0400

    initial coding

--- SurfaceServiceServer/Program.cs
+++ SurfaceServiceServer/Program.cs
@@ -9,12 +9,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name V1");
+});
 
 app.UseHttpsRedirection();
 
